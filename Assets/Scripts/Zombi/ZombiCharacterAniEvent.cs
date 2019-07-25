7c0ee51d63bae18b5507fd4ca2c44e0c14f26a7b
_Culter <we13245@gmail.com>
2019-07-25 19:58:53 +0900

    Make ZombiAnimation

---
+++ Assets/Scripts/Zombi/ZombiCharacterAniEvent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Zombi
+{
+    public class ZombiCharacterAniEvent : MonoBehaviour
+    {
+        #region Inspector
+        [Header("Component")]
+        [SerializeField] private ZombiCharacter m_ZombiCharacter;
+        #endregion
+
+        #region Event
+        //Animation Event
+        public void OnSpawnEnd()
+        {
+            m_ZombiCharacter.OnSpawnEnd();
+        }
+        public void OnAttackEnd()
+        {
+            m_ZombiCharacter.OnAttackEnd();
+        }
+        public void OnDieEnd()
+        {
+            m_ZombiCharacter.OnDieEnd();
+        }
+        #endregion
+    }
+}
